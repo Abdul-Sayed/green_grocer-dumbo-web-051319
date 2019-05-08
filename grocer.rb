f6e9dd75252fa38cfab6dbe28617196babd89726
Abdul <awsayed@alaska.edu>
2019-05-08 00:11:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,75 @@
 def consolidate_cart(cart)
-  # code here
+
+	entry_counts = cart.group_by{ |entry| entry}.map{ |item, details| [item, details.count] }
+	entry_counts.each { |elem| 
+
+		elem[0].each_key { |k|
+			cart.each { |entry|
+				entry.each{ |item, details|
+					if item == k 
+						details[:count] = elem[1]
+					end
+				}
+			}
+		}
+	}
+
+	intermediate = cart.uniq
+	return_obj = {}
+
+	intermediate.each { |elem|
+		elem.each { |k,v|
+			return_obj[k] = v
+		}
+	}
+
+	return return_obj
+
 end
 
+############################################################
+
 def apply_coupons(cart, coupons)
-  # code here
+
+	num_extra_coupons = 0
+
+	if coupons.uniq.length == coupons.length
+		puts "coupons does not contain duplicates"
+	else
+		print coupons
+		puts "";print"\n"
+		print coupons.uniq
+		puts "";print"\n"
+		duplicates = coupons.group_by { |e| e }.keep_if { |_, e| e.length > 1 }.map { |k, v| { k => v.length } } 
+		print duplicates
+		duplicates[0].each { |k,v| num_extra_coupons = v-1 }
+		puts "";print"\n"
+	end
+
+	puts num_extra_coupons
+
+	coupons.each { |coupon|
+		sale_item = coupon[:item]
+		num_sale_items = coupon[:num]    
+
+		if cart.has_key?(sale_item)
+			num_items = cart[sale_item][:count] 
+			cart[sale_item][:count] -= num_sale_items 
+
+			cart["#{sale_item} W/COUPON"] = { }.merge!( price: coupon[:cost], clearance: cart[coupon[:item]][:clearance], count: ( num_items / coupon[:num] ) + num_extra_coupons )
+		end
+	}
+
+	return cart
 end
+
+###########################################################
 
 def apply_clearance(cart)
   # code here
 end
+
+##########################################################
 
 def checkout(cart, coupons)
   # code here
